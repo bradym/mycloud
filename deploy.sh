4a80239fe563975d20b9d0a0f0619fe64f0889b8
Brady Mitchell <bradym@users.noreply.github.com>
2018-08-20 15:46:05 -0500

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+if [[ ! -z ${DEBUG_MODE+x} ]]; then
+    set -o xtrace
+    set -o functrace
+fi
+
+ENV_FILE=$(mktemp)
+
+function cleanup_before_exit () {
+    rm "$ENV_FILE"
+}
+
+trap cleanup_before_exit EXIT
+
+# Get parameters from AWS SSM Parameter Store and set variabes based on their names.
+aws ssm get-parameters-by-path --path / --with-decryption --recursive | jq -r '.Parameters[] | [.Name,.Value] | @tsv' |
+    while IFS=$'\t' read -r PARAM VALUE; do
+        NAME=$(echo "$PARAM" | sed 's/^.\{1\}//' | sed 's#/#_#')
+        echo "export $NAME='$VALUE'" >> "$ENV_FILE"
+    done
+
+# Where is this script running?
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Make the output folder if it doesn't exost
+mkdir -p "$DIR/_env"
+
+source "$ENV_FILE"
+
+# Render all of the environment variable templates
+for INPUT_FILE in _templates/*.env; do
+    OUTPUT_FILENAME="$DIR/_env/$(basename "$INPUT_FILE")"
+    envsubst < "$INPUT_FILE" > "$OUTPUT_FILENAME"
+done
+
+# Render the docker-compose template
+envsubst < _templates/docker-compose.tmpl.yaml > "$DIR/docker-compose.yaml"
+
+# Deploy all the things!
+docker-compose up -d --force-recreate
